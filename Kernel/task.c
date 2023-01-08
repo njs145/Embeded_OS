ba9e82c849b02bf4114afca6cff230e6e7799e0b
Nam JinSong <njs145@naver.com>
2023-01-08 13:52:07 +0900

    10.2 컨텍스트 복구하기

--- Kernel/task.c
+++ Kernel/task.c
@@ -11,6 +11,8 @@ static KernelTcb_t      sTask_list[MAX_TASK_NUM];
 static uint32_t         sAllocated_tcb_index;
 
 static KernelTcb_t*     Scheduler_round_robin_algorithm(void);
+static __attribute__ ((naked)) void Save_context(void);
+
 void Kernel_task_context_switching();
 
 void Kernel_task_scheduler(void)
@@ -93,4 +95,17 @@ static __attribute__ ((naked)) void Save_context(void)
     __asm__ ("LDR   r0, =sCurrent_tcb");
     __asm__ ("LDR   r0, [r0]");
     __asm__ ("STMIA r0!, {sp}");
+}
+
+static __attribute__ ((naked)) void Restore_context(void)
+{
+    /* restore next task stack pointer from the next TCB */
+    __asm__ ("LDR   r0, =sNext_tcb");
+    __asm__ ("LDR   r0, [r0]");
+    __asm__ ("LDMIA r0!, {sp}");
+    /* restore next task context from the next task stack */
+    __asm__ ("POP   {r0}");
+    __asm__ ("MSR   cpsr, r0");
+    __asm__ ("POP   {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9,r10, r11, r12}");
+    __asm__ ("POP   {pc}");
 }
